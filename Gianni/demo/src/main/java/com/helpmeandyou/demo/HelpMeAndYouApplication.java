59549db4e5b4731e93bc35a8dae50883fb09ed75
gianni <gianni.molinari@studenti.unimi.it>
2023-03-23 11:35:18 +0100

    inizializzata la mia parte con spring

---
+++ Gianni/demo/src/main/java/com/helpmeandyou/demo/HelpMeAndYouApplication.java
@@ -0,0 +1,13 @@
+package com.helpmeandyou.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpMeAndYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpMeAndYouApplication.class, args);
+	}
+
+}
