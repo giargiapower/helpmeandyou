37e6ebacfb67d78504c9e54c1d3603fd4f766fe4
gianni <gianni.molinari@studenti.unimi.it>
2023-03-24 11:28:42 +0100

    creata classe conto

--- Gianni/demo/src/main/java/com/HelpMeAndYou/demo/Model/Conto.java
+++ Gianni/demo/src/main/java/com/HelpMeAndYou/demo/Model/Conto.java
@@ -4,19 +4,51 @@ package com.HelpMeAndYou.demo.Model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "contocorrente")
+@Table(name = "banca")
 public class Conto {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private long conto_corrente;
 
-    @Column(name = "name")
-    private String name;
+    @Column(name = "numero_di_conto")
+    private int numero_di_conto;
 
-    @Column(name = "age")
-    private int age;
+
+    @Column(name = "saldo")
+    private float saldo;
 
     public Conto() {
     }
+    public Conto(int numero_di_conto, float saldo) {
+        this.numero_di_conto = numero_di_conto;
+        this.saldo = saldo;
+    }
 
+    public long getId() {
+        return conto_corrente;
+    }
+
+    public void setConto(int numero_di_conto) {
+        this.numero_di_conto = numero_di_conto;
+    }
+
+    public int getConto() {
+        return this.numero_di_conto;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+    public void addSaldo(float saldo) {
+        this.saldo += saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "com.example.Spring2.Customer [id=" + conto_corrente + ", numero di conto=" + numero_di_conto + ", saldo=" + saldo + "]";
+    }
 }
