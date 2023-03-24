ced2e293c29a4071b4e561eae46b1c6a3bacea5c
gianni <gianni.molinari@studenti.unimi.it>
2023-03-24 11:51:52 +0100

    creato repository che gestisce interazione con DB e controller che gestisce le api per interfacciarci da esterno a repository

--- Gianni/demo/src/main/java/com/HelpMeAndYou/demo/Repository/ContoRepository.java
+++ Gianni/demo/src/main/java/com/HelpMeAndYou/demo/Repository/ContoRepository.java
@@ -1,6 +1,11 @@
 package com.HelpMeAndYou.demo.Repository;
 
 
-public interface ContoRepository {
+import com.HelpMeAndYou.demo.Model.Conto;
+import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
+public interface ContoRepository extends CrudRepository<Conto, Long> {
+    List<Conto> findByConto(int conto);
 }
