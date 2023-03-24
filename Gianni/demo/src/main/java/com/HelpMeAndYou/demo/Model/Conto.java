f0dfd86d18c1535541e41fd90b1e18c7cac1de13
gianni <gianni.molinari@studenti.unimi.it>
2023-03-24 12:35:06 +0100

    refactoring Conto per problemi con connessione al db

--- Gianni/demo/src/main/java/com/HelpMeAndYou/demo/Model/Conto.java
+++ Gianni/demo/src/main/java/com/HelpMeAndYou/demo/Model/Conto.java
@@ -1,7 +1,11 @@
 package com.HelpMeAndYou.demo.Model;
 
-
-import jakarta.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "banca")
