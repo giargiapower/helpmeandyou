272b452dadd88795ba3b72fd13b579ff0feb92d6
gianni <gianni.molinari@studenti.unimi.it>
2023-04-07 16:41:03 +0200

    debuggati Amministratore, Account e Segnalazioni, non ancora del tutto completati

--- Gianni/Segnalazioni_1/src/main/java/com/example/Segnalazioni_1/Controller/AccountController.java
+++ Gianni/Segnalazioni_1/src/main/java/com/example/Segnalazioni_1/Controller/AccountController.java
@@ -1,9 +1,10 @@
 package com.example.Segnalazioni_1.Controller;
 
-
+import com.example.Segnalazioni_1.Model.Segnalazioni;
 import com.example.Segnalazioni_1.Model.Account;
 import com.example.Segnalazioni_1.Model.Segnalazioni;
 import com.example.Segnalazioni_1.Repository.AccountRepository;
+import com.example.Segnalazioni_1.Repository.SegnalazioniRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,72 +19,38 @@ import java.util.Optional;
 @RequestMapping("/api/utenti")
 public class AccountController {
     @Autowired
-    AccountRepository repository;
-///////////////////////////////////////////////////////////////////////////////////////////////
-    // DA SPOSTARE SU MICROSERVIZIO AMMINISTRATORE
-    // metodo per amministratore, ritorna tutti gli account in stato di apertura (da approvare)
-    @GetMapping("/da_approvare/list")
-    public List<Account> getAllAccountToApprove() {
+    SegnalazioniRepository repository;
 
-        List<Account> accounts = new ArrayList<>();
-        repository.findAllByStato("da_approvare").forEach(accounts::add);
-        return accounts;
-    }
+    @Autowired
+    AccountRepository a_repository;
 
-    // metodi per amministratore approva o disapprova la creazione di un account in base ai parametri dell'account
-    // da valutare se aggiungere fattori di sicurezza tipo controllare se chi richiama il microservizio è un amministratore valido
-    @PutMapping("/da_approvare/approva/{id}")
-    public ResponseEntity<Account> approvaAccount(@PathVariable("id") long id) {
 
-        Optional<Account> c = repository.findById(id);
+    @PostMapping(value = "/segnalazione/create")
+    public Segnalazioni postSegnalazione(@RequestBody Segnalazioni segnalazione) {
 
-        if (c.isPresent()) {
-            Account _account = c.get();
-            _account.setStato("approvato");
-
-            return new ResponseEntity<>(repository.save(_account), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @PutMapping("/da_approvare/disapprova/{id}")
-    public ResponseEntity<Account> disapprovaAccount(@PathVariable("id") long id) {
-
-        Optional<Account> c = repository.findById(id);
-
-        if (c.isPresent()) {
-            Account _account = c.get();
-            _account.setStato("bloccato");
-
-            return new ResponseEntity<>(repository.save(_account), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        Segnalazioni s = repository.save(new Segnalazioni(segnalazione.getTitolo() , segnalazione.getDescrizione() , segnalazione.getTipologia(), segnalazione.getCreatore() , segnalazione.getSegnalato()));
+        return s;
     }
 
     // crea l'account
     @PostMapping(value = "/create")
     public ResponseEntity<String> postAccount(@RequestBody Account account) {
         // Verifica se l'account è già presente nel database
-        Account accountEsistente = repository.findByEmail(account.getEmail());
+        Account accountEsistente = a_repository.findByEmail(account.getEmail());
         if (accountEsistente != null) {
             // L'account è già presente, restituisce una segnalazione corrispondente
             return ResponseEntity.badRequest().body("Email già presente nel database");
         } else {
             // L'account non è presente, salva l'account nel database
-            repository.save(account);
+            a_repository.save(account);
             // Restituisce una segnalazione di successo
             return ResponseEntity.ok("ok");
         }
     }
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
 
 }
+
+
+
+
---
+++ Gianni/Segnalazioni_1/src/main/java/com/example/Segnalazioni_1/Controller/AmministatoreController.java
@@ -0,0 +1,105 @@
+package com.example.Segnalazioni_1.Controller;
+
+import com.example.Segnalazioni_1.Model.Account;
+import com.example.Segnalazioni_1.Model.Segnalazioni;
+import com.example.Segnalazioni_1.Repository.AccountRepository;
+import com.example.Segnalazioni_1.Repository.AmministratoreRepository;
+import com.example.Segnalazioni_1.Repository.SegnalazioniRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/amministratore")
+public class AmministatoreController {
+
+    @Autowired
+    AmministratoreRepository repository;
+
+    @Autowired
+    SegnalazioniRepository s_repository;
+
+    // metodo per amministratore, ritorna tutti gli account in stato di apertura (da approvare)
+    @GetMapping("/da_approvare/list")
+    public List<Account> getAllAccountToApprove() {
+
+        List<Account> accounts = new ArrayList<>();
+        repository.findAllByStato("da_approvare").forEach(accounts::add);
+        return accounts;
+    }
+
+    // metodi per amministratore approva o disapprova la creazione di un account in base ai parametri dell'account
+    // da valutare se aggiungere fattori di sicurezza tipo controllare se chi richiama il microservizio è un amministratore valido
+    @PutMapping("/da_approvare/approva/{id}")
+    public ResponseEntity<Account> approvaAccount(@PathVariable("id") long id) {
+
+        Optional<Account> c = repository.findById(id);
+
+        if (c.isPresent()) {
+            Account _account = c.get();
+            _account.setStato("approvato");
+
+            return new ResponseEntity<>(repository.save(_account), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/da_approvare/disapprova/{id}")
+    public ResponseEntity<Account> disapprovaAccount(@PathVariable("id") long id) {
+
+        Optional<Account> c = repository.findById(id);
+
+        if (c.isPresent()) {
+            Account _account = c.get();
+            _account.setStato("bloccato");
+
+            return new ResponseEntity<>(repository.save(_account), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/segnalazione/{id}")
+    public Segnalazioni getSegnalazione(@PathVariable("id") long id) {
+        Optional<Segnalazioni> s = s_repository.findById(id);
+
+        if (s.isPresent()) {
+            Segnalazioni _segnalazione = s.get();
+            return _segnalazione;
+        } else {
+            return null;
+        }
+    }
+
+    @GetMapping("/segnalazione/list")
+    public List<Segnalazioni> getAllSegnalazioni() {
+        System.out.println("Get all Segnalations...");
+
+        List<Segnalazioni> segnalazione = new ArrayList<>();
+        s_repository.findAll().forEach(segnalazione::add);
+
+        return segnalazione;
+    }
+
+    @DeleteMapping("/segnalazione/delete/{id}")
+    public ResponseEntity<Segnalazioni> deleteSegnalazione(@PathVariable("id") long id) {
+        Optional<Segnalazioni> c = s_repository.findById(id);
+        if (c.isPresent()) {
+            s_repository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
+}
