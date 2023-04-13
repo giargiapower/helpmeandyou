ecad0c561923a1b27069198819e2ba5e996a8ed2
gianni <gianni.molinari@studenti.unimi.it>
2023-04-13 10:36:35 +0200

    creato repo per gestione credenziali

---
+++ Gianni/GestoreCredenziali/src/main/java/com/GestoreCredenziali/GestoreCredenziali/GestoreCredenzialiApplication.java
@@ -0,0 +1,13 @@
+package com.GestoreCredenziali.GestoreCredenziali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestoreCredenzialiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestoreCredenzialiApplication.class, args);
+	}
+
+}
