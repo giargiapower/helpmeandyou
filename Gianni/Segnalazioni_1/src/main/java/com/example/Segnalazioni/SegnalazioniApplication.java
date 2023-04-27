cb2577cef088a833b7f7268207f526033b5d6b71
gianni <gianni.molinari@studenti.unimi.it>
2023-04-27 11:07:20 +0200

    api gateway + eureka server funzionante e dockerizzato: per il momento riesce a reindirizzare solo al microservizio delle segnalazioni

--- Gianni/Segnalazioni_1/src/main/java/com/example/Segnalazioni/SegnalazioniApplication.java
+++ Gianni/Segnalazioni_1/src/main/java/com/example/Segnalazioni/SegnalazioniApplication.java
@@ -3,11 +3,9 @@ package com.example.Segnalazioni;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-		FileStorageProperties.class
-})
 public class SegnalazioniApplication {
 
 	public static void main(String[] args) {
