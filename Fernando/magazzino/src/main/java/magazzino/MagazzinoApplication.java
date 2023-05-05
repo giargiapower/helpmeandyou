eeea42aeee4399f70bc30fbf4a4a0ad825376429
gianni <gianni.molinari@studenti.unimi.it>
2023-05-05 11:51:41 +0200

    magazzino e requesthelp collegati all'api gateway porta 9000

--- Fernando/magazzino/src/main/java/magazzino/MagazzinoApplication.java
+++ Fernando/magazzino/src/main/java/magazzino/MagazzinoApplication.java
@@ -3,7 +3,7 @@ package magazzino;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration.class})
 public class MagazzinoApplication {
 
     public static void main(String[] args) {
--- requestshelp/src/main/java/requests/RichiestaAiuto.java
+++ requestshelp/src/main/java/requests/RichiestaAiuto.java
@@ -2,7 +2,9 @@ package requests;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(exclude={org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration.class})
 public class RichiestaAiuto {
 
